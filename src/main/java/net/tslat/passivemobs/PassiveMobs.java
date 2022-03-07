8605c3827e46b6e8cf8164895853d94c02ae9c92
Tslat <tslatmail@gmail.com>
2022-03-08 05:42:28 +1000

    Add tag blacklist

--- src/main/java/net/tslat/passivemobs/PassiveMobs.java
+++ src/main/java/net/tslat/passivemobs/PassiveMobs.java
@@ -1,7 +1,11 @@
 package net.tslat.passivemobs;
 
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
+import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.tags.ITag;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
@@ -12,15 +16,14 @@ import static net.tslat.passivemobs.PassiveMobs.MOD_ID;
 @Mod(MOD_ID)
 public class PassiveMobs {
 	public static final String MOD_ID = "passivemobs";
-	public static final String VERSION = "1.1";
-	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+	public static final Lazy<ITag.INamedTag<EntityType<?>>> PACIFICATION_IMMUNE_TAG = Lazy.of(() -> EntityTypeTags.bind("immune_to_pacification"));
 
 	public PassiveMobs() {
 		MinecraftForge.EVENT_BUS.addListener(this::entityTarget);
 	}
 
 	private void entityTarget(final LivingSetAttackTargetEvent ev) {
-		if (ev.getTarget() != null && ev.getEntityLiving() instanceof MobEntity)
+		if (ev.getTarget() != null && ev.getEntityLiving() instanceof MobEntity && !ev.getEntityLiving().getType().is(PACIFICATION_IMMUNE_TAG.get()))
 			((MobEntity)ev.getEntityLiving()).setTarget(null);
 	}
 }
