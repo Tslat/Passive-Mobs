18609638c27561cdb92faf222862f14bfbac45f8
Tslat <tslatmail@gmail.com>
2023-06-09 18:28:39 +1000

    Update to 1.20

--- Fabric/src/main/java/net/tslat/passivemobs/mixin/MobMixin.java
+++ Fabric/src/main/java/net/tslat/passivemobs/mixin/MobMixin.java
@@ -3,12 +3,12 @@ package net.tslat.passivemobs.mixin;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.tslat.passivemobs.Constants;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.annotation.Nullable;
 
 @Mixin(Mob.class)
 public class MobMixin {
