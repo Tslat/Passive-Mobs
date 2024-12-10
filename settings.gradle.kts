6fe1361a8d02af10ba44b325a6cd5c689c7f8270
Tslat <tslatmail@gmail.com>
2024-12-11 04:45:13 +1000

    Update to 1.21.4 and new build setup

---
+++ settings.gradle.kts
@@ -0,0 +1,82 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+		
+		// Fabric
+        exclusiveContent {
+            forRepository {
+                maven {
+                    name = "Fabric"
+                    url = uri("https://maven.fabricmc.net")
+                }
+            }
+            filter {
+                includeGroup("net.fabricmc")
+                includeGroup("fabric-loom")
+            }
+        }
+		
+		// NeoForge
+        exclusiveContent {
+            forRepository {
+                maven {
+                    name = "NeoForge"
+                    url = uri("https://maven.neoforged.net/releases")
+                }
+            }
+            filter {
+                includeGroupAndSubgroups("net.neoforged")
+                includeGroup("codechicken")
+            }
+        }
+		
+		// Forge
+        exclusiveContent {
+            forRepository {
+                maven {
+                    name = "Forge"
+                    url = uri("https://maven.minecraftforge.net/")
+                }
+            }
+            filter {
+                includeGroupAndSubgroups("net.minecraftforge")
+            }
+        }
+		
+		// Mixin
+        exclusiveContent {
+            forRepository {
+                maven {
+                    name = "SpongeForge"
+                    url = uri("https://repo.spongepowered.org/repository/maven-public")
+                }
+            }
+            filter {
+                includeGroupAndSubgroups("org.spongepowered")
+            }
+        }
+
+		// Parchment
+        exclusiveContent {
+            forRepository {
+                maven {
+                    name = "Parchment"
+                    url = uri("https://maven.parchmentmc.org")
+                }
+            }
+            filter {
+                includeGroupAndSubgroups("org.parchmentmc")
+            }
+        }
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "passivemobs"
+include("common")
+include("fabric")
+include("forge")
+include("neoforge")
